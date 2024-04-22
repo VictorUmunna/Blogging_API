492a4454fc70675f502c37a99a3f3d8188aa2b73
Victor Umunna <umunnavictor01@gmail.com>
2024-04-22 22:45:37 +0100

    update test and utils folders

---
+++ utils/appError.js
@@ -0,0 +1,12 @@
+class appError extends Error {
+    constructor(message, statusCode) {
+      super(message);
+      this.statusCode = statusCode;
+      this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+      this.isOperational = true;
+      // to log the error stack tree in the error handler(error.stack)
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+  
+  module.exports = appError;
